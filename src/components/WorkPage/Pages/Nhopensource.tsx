42838e9bfa2bfedb025688cf8d508edc4d408204
DESKTOP-0H8IHQH\darum <darum>
2025-12-08 11:14:17 +0900

    [이동현] 포트폴리오 업데이트

--- src/components/WorkPage/Pages/Nhopensource.tsx
+++ src/components/WorkPage/Pages/Nhopensource.tsx
@@ -1,107 +1,87 @@
-import React, { useEffect } from "react"
-import { WorkContentItem } from "./types/types"
-import Prism from "prismjs"
-import "prismjs/themes/prism-tomorrow.css"
-import "prismjs/components/prism-typescript"
-import "prismjs/components/prism-jsx"
+import { WorkContentItem } from "./types/types";
 interface PortfoliopageProps {
-  item: WorkContentItem
+	item: WorkContentItem;
 }
 
 const Nhopensource: React.FC<PortfoliopageProps> = () => {
-  useEffect(() => {
-    Prism.highlightAll()
-  }, [])
+	return (
+		<div className="modalContent">
+			<div className="projectImage">
+				<img src="/assets/images/main/nhopengifImage1.gif" alt="프로젝트 화면 이미지" />
+			</div>
+			<div className="introText">
+				<p>
+					금융권과 같은 업계의 철저한 보안성과 안정성을 위해 외부망에 대한 규제가 엄격하게 이루어집니다.
+					<br className="mob-hidden" />
+					때문에 우리가 외부에서 정말 손쉽게 구할 수 있는 정보, 검색, 다양한 웹 브라우저 , 라이브러리 ,
+					프레임워크 등 내부로의
+					<br className="mob-hidden" />
+					반입이 제한이 걸립니다. 정말 필요한 경우의 문서 및 파일의 경우 철저한 절차를 통하여 내부로의 반입이
+					이루어지는데 '오픈소스관리포털'은 이를 좀 더 체계적이면서 효울적으로 절차를 진행하고자 취지에서
+					만들어진 내부 시스템입니다.
+				</p>
+			</div>
+			<p className="causeText">* 프로젝트 보안상 코드공개가 어려운 점 양해부탁드립니다.</p>
+			<div className="characteristic content">
+				<h3 className="contentTit">특징</h3>
+				<ul className="listStyle1">
+					<li>
+						프론트단 100% 개인구축
+						<ul className="listStyle2">
+							<li>
+								프로젝트 전 페이지의 화면 설계 및 UI 구현을 단독으로 담당하여 초기 구조 설계부터
+								컴포넌트 개발<br></br> 유지보수 체계 구축까지 전체 화면 개발을 주도적으로 수행했습니다.
+							</li>
+						</ul>
+					</li>
+					<li>
+						공통 UI 컴포넌트 모듈화 관리
+						<ul className="listStyle2">
+							<li>
+								<p>
+									반복 작업을 최대한 줄이기 위해서 버튼,팝업,검색영역 등 최소한의 컴포넌트 형태로
+									컴포넌트 설계
+								</p>
+							</li>
+							<li>
+								<p>
+									단일화된 컴포넌트로 서로 다른 페이지의 slot, props, emit 기반 데이터 값에 따른
+									컴포넌트 구성
+								</p>
+							</li>
+						</ul>
+					</li>
+					<li>
+						효과적인 페이지 관리를 위한 Vue Router 활용
+						<ul className="listStyle2">
+							<li>전체 페이지 관리를 위한 Router 관리</li>
+							<li>백엔드 개발과 협의하여 올바른 RESTful API 구조에 맞게 Router 설정에 노력을 기울임</li>
+							<li>
+								해당 페이지의 데이터에 따라 Router 페이지 전환시 일관된 데이터가 들어올 수 있도록 탄탄한
+								코드작업에 노력을 기울임
+							</li>
+						</ul>
+					</li>
+					<li>
+						Scss를 활용한 컴포넌트 모듈화
+						<ul className="listStyle2">
+							<li>반복되는 css 코드를 최소한으로 줄이는데에 노력을 기울임</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
 
-  return (
-    <div className="modalContent">
-      <div className="projectImage">
-        <img
-          src="/assets/images/main/nhopengifImage1.gif"
-          alt="프로젝트 화면 이미지"
-        />
-      </div>
-      <div className="introText">
-        <p>
-          금융권과 같은 업계의 철저한 보안성과 안정성을 위해 외부망에 대한
-          규제가 엄격하게 이루어집니다.
-          <br className="mob-hidden" />
-          때문에 우리가 외부에서 정말 손쉽게 구할 수 있는 정보, 검색, 다양한 웹
-          브라우저 , 라이브러리 , 프레임워크 등 내부로의
-          <br className="mob-hidden" />
-          반입이 제한이 걸립니다. 정말 필요한 경우의 문서 및 파일의 경우 철저한
-          절차를 통하여 내부로의 반입이 이루어지는데 '오픈소스관리포털'은 이를
-          좀 더 체계적이면서 효울적으로 절차를 진행하고자 취지에서 만들어진 내부
-          시스템입니다.
-        </p>
-      </div>
-      <p className="causeText">
-        * 프로젝트 보안상 코드공개가 어려운 점 양해부탁드립니다.
-      </p>
-      <div className="characteristic content">
-        <h3 className="contentTit">특징</h3>
-        <ul className="listStyle1">
-          <li>
-            프론트단 100% 개인구축
-            <ul className="listStyle2">
-              <li>
-                프로젝트 전 페이지의 화면 설계 및 UI 구현을 단독으로 담당하여
-                초기 구조 설계부터 컴포넌트 개발<br></br> 유지보수 체계 구축까지
-                전체 화면 개발을 주도적으로 수행했습니다.
-              </li>
-            </ul>
-          </li>
-          <li>
-            공통 UI 컴포넌트 모듈화 관리
-            <ul className="listStyle2">
-              <li>
-                <p>
-                  반복 작업을 최대한 줄이기 위해서 버튼,팝업,검색영역 등
-                  최소한의 컴포넌트 형태로 컴포넌트 설계
-                </p>
-              </li>
-              <li>
-                <p>
-                  단일화된 컴포넌트로 서로 다른 페이지의 slot, props, emit 기반
-                  데이터 값에 따른 컴포넌트 구성
-                </p>
-              </li>
-            </ul>
-          </li>
-          <li>
-            효과적인 페이지 관리를 위한 Vue Router 활용
-            <ul className="listStyle2">
-              <li>전체 페이지 관리를 위한 Router 관리</li>
-              <li>
-                백엔드 개발과 협의하여 올바른 RESTful API 구조에 맞게 Router
-                설정에 노력을 기울임
-              </li>
-              <li>
-                해당 페이지의 데이터에 따라 Router 페이지 전환시 일관된 데이터가
-                들어올 수 있도록 탄탄한 코드작업에 노력을 기울임
-              </li>
-            </ul>
-          </li>
-          <li>
-            Scss를 활용한 컴포넌트 모듈화
-            <ul className="listStyle2">
-              <li>반복되는 css 코드를 최소한으로 줄이는데에 노력을 기울임</li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+			<div className="skill content">
+				<h3 className="contentTit">사용된 기술</h3>
+				<ul className="skillTree">
+					<li>Vue3</li>
+					<li>Vite</li>
+					<li>TypeScript</li>
+					<li>Scss</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
 
-      <div className="skill content">
-        <h3 className="contentTit">사용된 기술</h3>
-        <ul className="skillTree">
-          <li>Vue3</li>
-          <li>Vite</li>
-          <li>TypeScript</li>
-          <li>Scss</li>
-        </ul>
-      </div>
-    </div>
-  )
-}
-
-export default Nhopensource
+export default Nhopensource;
--- src/components/WorkPage/Pages/NiceBizinfo.tsx
+++ src/components/WorkPage/Pages/NiceBizinfo.tsx
@@ -1,118 +1,100 @@
-import React, { useEffect } from "react"
-import Prism from "prismjs"
-import "prismjs/themes/prism-tomorrow.css"
-import "prismjs/components/prism-typescript"
-import "prismjs/components/prism-jsx"
-
-import { WorkContentItem } from "./types/types"
+import { WorkContentItem } from "./types/types";
 
 interface PortfoliopageProps {
-  item: WorkContentItem
+	item: WorkContentItem;
 }
 
 const NiceBizinfo: React.FC<PortfoliopageProps> = () => {
-  useEffect(() => {
-    Prism.highlightAll()
-  }, [])
+	return (
+		<div className="modalContent">
+			<div className="introText">
+				<p>
+					기업 관련 분석 보고서, 재무제표, 기업 신용 등 사업자 관련 정보를 손쉽게 검색하고 열람할 수 있도록
+					하며, 보고서 구매 서비스를 일원화하여 제공합니다. 최신 UI/UX 트렌드를 반영한 웹사이트를 구축함으로써
+					사용자의 구매 경험을 향상시키고, 이를 통해 가치를 창출하여 클라이언트에게 효과적인 서비스를 제공하는
+					것을 목표로 합니다.
+				</p>
+			</div>
+			<p className="causeText">* 프로젝트 보안상 코드공개가 어려운 점 양해부탁드립니다.</p>
 
-  return (
-    <div className="modalContent">
-      <div className="introText">
-        <p>
-          기업 관련 분석 보고서, 재무제표, 기업 신용 등 사업자 관련 정보를
-          손쉽게 검색하고 열람할 수 있도록 하며, 보고서 구매 서비스를 일원화하여
-          제공합니다. 최신 UI/UX 트렌드를 반영한 웹사이트를 구축함으로써
-          사용자의 구매 경험을 향상시키고, 이를 통해 가치를 창출하여
-          클라이언트에게 효과적인 서비스를 제공하는 것을 목표로 합니다.
-        </p>
-      </div>
-      <p className="causeText">
-        * 프로젝트 보안상 코드공개가 어려운 점 양해부탁드립니다.
-      </p>
+			<div className="characteristic content">
+				<h3 className="contentTit">특징</h3>
+				<ul className="listStyle1">
+					<li>
+						웹 접근성 심사마크 단독 취득
+						<ul className="listStyle2">
+							<li>
+								<p>접근성 인증 완료</p>
+								<img
+									src="/assets/images/contents/bizinfo-image01.png"
+									alt="웹접근성마크 인증 이미지"
+									className="wd-100"
+								/>
+							</li>
+							<li>
+								<p>
+									Quasar 기본 컴포넌트(q-btn, q-radio, q-input 등)는 국내(K-WCAG) 접근성 규정에 일부
+									미흡한 부분이 있어, 직접 커스텀 컴포넌트를 제작하고 내부 로직을 수정하여 접근성을
+									강화시켰습니다.{" "}
+								</p>
+							</li>
+							<li>
+								<p>
+									Nuxt 3의 SSR + SPA 특성상 페이지 이동 시 스크롤 위치가 유지되는 특성이 있어 접근성
+									및 사용자 경험 측면에서 초기 <br /> 포커스 및 스크롤 제어가 필요했습니다.
+								</p>
+							</li>
+						</ul>
+					</li>
+					<li>
+						SEO(검색엔진최적화)에 최적화 된 Nuxt3 프레임워크 채택
+						<ul className="listStyle2">
+							<li>
+								Vue 3 + Composition API 기반으로 SSR, SSG, CSR, On-demand Revalidation(ISR), Hybrid
+								Rendering 등 다양한 렌더링 전략을 유연하게 지원
+							</li>
+						</ul>
+					</li>
+					<li>
+						TypeScript 완전 통합
+						<ul className="listStyle2">
+							<li>
+								컴포넌트의 Props와 Emits를 명세화하여 잘못된 타입 전달을 빌드 시 즉시 확인하고, 재사용
+								컴포넌트의 UI 설계에서 의도치 않은 사용 실수를 방지했습니다.
+							</li>
+							<li>
+								reactive와 ref의 데이터 구조를 타입으로 보장하여 잘못된 속성 접근이나 변경을 사전에
+								차단했습니다.
+							</li>
+						</ul>
+					</li>
+					<li>
+						파일 기반 라우팅
+						<ul className="listStyle2">
+							<li>
+								RESTful API 경로와 프론트 URL 경로가 일관되게 설계 되어 백엔드와 API 스펙 협의가
+								원활하였습니다.
+							</li>
+							<li>
+								웹 접근성을 목표로 한 프로젝트로 SEO 페이지 경로 설계를 접근성에 맞게 직접 구성하였으며,
+								페이지 경로 설계와 SEO작업에 기여를 하였습니다.
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
 
-      <div className="characteristic content">
-        <h3 className="contentTit">특징</h3>
-        <ul className="listStyle1">
-          <li>
-            웹 접근성 심사마크 단독 취득
-            <ul className="listStyle2">
-              <li>
-                <p>접근성 인증 완료</p>
-                <img
-                  src="/assets/images/contents/bizinfo-image01.png"
-                  alt="웹접근성마크 인증 이미지"
-                  className="wd-100"
-                />
-              </li>
-              <li>
-                <p>
-                  Quasar 기본 컴포넌트(q-btn, q-radio, q-input 등)는
-                  국내(K-WCAG) 접근성 규정에 일부 미흡한 부분이 있어, 직접
-                  커스텀 컴포넌트를 제작하고 내부 로직을 수정하여 접근성을
-                  강화시켰습니다.{" "}
-                </p>
-              </li>
-              <li>
-                <p>
-                  Nuxt 3의 SSR + SPA 특성상 페이지 이동 시 스크롤 위치가
-                  유지되는 특성이 있어 접근성 및 사용자 경험 측면에서 초기{" "}
-                  <br /> 포커스 및 스크롤 제어가 필요했습니다.
-                </p>
-              </li>
-            </ul>
-          </li>
-          <li>
-            SEO(검색엔진최적화)에 최적화 된 Nuxt3 프레임워크 채택
-            <ul className="listStyle2">
-              <li>
-                Vue 3 + Composition API 기반으로 SSR, SSG, CSR, On-demand
-                Revalidation(ISR), Hybrid Rendering 등 다양한 렌더링 전략을
-                유연하게 지원
-              </li>
-            </ul>
-          </li>
-          <li>
-            TypeScript 완전 통합
-            <ul className="listStyle2">
-              <li>
-                컴포넌트의 Props와 Emits를 명세화하여 잘못된 타입 전달을 빌드 시
-                즉시 확인하고, 재사용 컴포넌트의 UI 설계에서 의도치 않은 사용
-                실수를 방지했습니다.
-              </li>
-              <li>
-                reactive와 ref의 데이터 구조를 타입으로 보장하여 잘못된 속성
-                접근이나 변경을 사전에 차단했습니다.
-              </li>
-            </ul>
-          </li>
-          <li>
-            파일 기반 라우팅
-            <ul className="listStyle2">
-              <li>
-                RESTful API 경로와 프론트 URL 경로가 일관되게 설계 되어 백엔드와
-                API 스펙 협의가 원활하였습니다.
-              </li>
-              <li>
-                웹 접근성을 목표로 한 프로젝트로 SEO 페이지 경로 설계를 접근성에
-                맞게 직접 구성하였으며, 페이지 경로 설계와 SEO작업에 기여를
-                하였습니다.
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+			<div className="skill content">
+				<h3 className="contentTit">사용된 기술</h3>
+				<ul className="skillTree">
+					<li>Nuxt3</li>
+					<li>Quasar</li>
+					<li>TypeScript</li>
+					<li>Scss</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
 
-      <div className="skill content">
-        <h3 className="contentTit">사용된 기술</h3>
-        <ul className="skillTree">
-          <li>Nuxt3</li>
-          <li>Quasar</li>
-          <li>TypeScript</li>
-          <li>Scss</li>
-        </ul>
-      </div>
-    </div>
-  )
-}
-
-export default NiceBizinfo
+export default NiceBizinfo;
